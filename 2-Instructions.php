ebc5b0406ea67593417fbdd34116c71746a1c414
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-12-30 13:24:04 +0100

    Updated so that we use headers now

--- 2-Instructions.php
+++ 2-Instructions.php
@@ -6,8 +6,10 @@
  <li>The data should have format: <b>A1,A2,A3,B1,B2,B3,label</b>, or <b>A1,A2,A3,B1,B2,B3</b>.</li>
  <li>Each CDR chain have a different maximum length limit, with the standard uppercase 20 amino acid alphabet:</li>
  <br><b>A C D E F G H I K L M N P Q R S T V W Y</b>
- <li>The input can be submitted with or without labels. Labels can be any specificity to a known peptide or the source of a sample, and are not used during clustering</li>
- <li>The input needs to be <b>comma separated, with no headers.</b></li>
+ <li>The input needs to be <b>comma separated, with headers: A1,A2,A3,B1,B2,B3 </b></li>
+ <li>The input can be submitted with or without labels. Labels can be any specificity to a known peptide or the source of a sample, and are not used during clustering.</li>
+ <li>If labels are provided, the column header must be A1,A2,A3,B1,B2,B3,label
+ <li>Additional columns that will not be used by the program (for example indices, etc.) can be added, but only columns with the column names A1,A2,A3,B1,B2,B3 are required.</li>
  <br><li>Load Example data on the Submission page to ensure your data format is correct.</li>
 </ul>
 <a href="/services/TCRcluster-1.0/CDR_lengths.html" target="_blank">List of supported CDR lengths</a><br>
