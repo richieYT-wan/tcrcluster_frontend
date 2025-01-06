9111c7f95209d453e2dee64d6f6b9dc65e8cafef
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2025-01-06 13:31:16 +0100

    Added datasets for download

--- 6-Evaluation_data_sets.php
+++ 6-Evaluation_data_sets.php
@@ -7,69 +7,32 @@
 
 </h2>
 <p>
-Here, you will find the data set used for evaluation of NetMHCpan-4.1 and NetMHCIIpan-4.0 methods.
-
+Here, you will find the data set used for training and evaluating the TCRcluster-1.0.
 <hr>
 
-<h2>NetMHCpan-4.1</h2>
+<h2>TCRcluster-1.0</h2>
 
-<h3>CD8 Epitope data set</h3>
+<h3>Training data</h3>
+<i>Only partitions 2,3,4 were used for training. Partition 0 was used as the validation and partition 1 was used as the held-out test set.</i>
 <p>
-<a target=_blank HREF="./suppl/CD8_epitopes/CD8_epitopes.fsa" target=_blank>CD8_epitopes.fsa</a> 
+<a target=_blank HREF="./supp/TCRcluster_train_data_all_partitions_binders_only.csv">All partitions, binders only</a>
+<a target=_blank HREF="./supp/TCRcluster_train_data_all_partitions_with_swap_negs.csv">All partitions, with swapped negatives</a>
 
-<h3>MS Ligands</h3>
+<h3>Evaluation data</h3>
+<i>Only partitions 0 and 1 were used for evaluating.
 <p>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:02">HLA-A02:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:05">HLA-A02:05</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:06">HLA-A02:06</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:11">HLA-A02:11</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A11:01">HLA-A11:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A23:01">HLA-A23:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A25:01">HLA-A25:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A26:01">HLA-A26:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A30:01">HLA-A30:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A30:02">HLA-A30:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A32:01">HLA-A32:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A33:01">HLA-A33:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A66:01">HLA-A66:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A68:01">HLA-A68:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B07:02">HLA-B07:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B08:01">HLA-B08:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B14:02">HLA-B14:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:01">HLA-B15:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:02">HLA-B15:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:03">HLA-B15:03</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:17">HLA-B15:17</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B18:01">HLA-B18:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B35:03">HLA-B35:03</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B37:01">HLA-B37:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B38:01">HLA-B38:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B40:01">HLA-B40:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B40:02">HLA-B40:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B45:01">HLA-B45:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B46:01">HLA-B46:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B53:01">HLA-B53:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B58:01">HLA-B58:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C03:03">HLA-C03:03</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C05:01">HLA-C05:01</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C07:02">HLA-C07:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C08:02">HLA-C08:02</a><br>
-<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C12:03">HLA-C12:03</a><br>
+<a target=_blank HREF="./supp/TCRcluster_17peptides_eval_partitions_binders_only.csv">17 peptides subset, binders only</a><br>
+<a target=_blank HREF="./supp/TCRcluster_17peptides_eval_partitions_with_swap_negs.csv">17 peptides subset, with swapped negatives</a><br>
+<
 
-
-<h2>NetMHCIIpan-4.0</h2>
-<p>
-<a target=_blank HREF="./suppl/CD4_epitopes/CD4_epitopes.fsa">CD4_epitopes.fsa</a>
-
-<hr>
 
 <h2>References</h2>
 
 <p>
-<b> NetMHCpan-4.1 and NetMHCIIpan-4.0: Improved predictions of MHC antigen presentation by concurrent motif deconvolution and integration of MS MHC eluted ligand data
-</b><br>Submitted 2020.
- 
+<b>...
+</b><br>Submitted 2025.
+
 <!-- SLUT INDHOLD -->
 
 <!-- footer -->
-            
+
