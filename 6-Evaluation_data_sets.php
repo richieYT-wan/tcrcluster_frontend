cc7470cfbf4e0d1a2a3a874baaf35ef98e968bd9
richieYT-wan <richieytwan@yahoo.fr>
2025-01-06 12:58:31 +0100

    Added download page

---
+++ 6-Evaluation_data_sets.php
@@ -0,0 +1,75 @@
+
+
+<!-- INDHOLD -->
+<h1>Supplementary material</h1>
+<h1></h1>
+<h2>
+
+</h2>
+<p>
+Here, you will find the data set used for evaluation of NetMHCpan-4.1 and NetMHCIIpan-4.0 methods.
+
+<hr>
+
+<h2>NetMHCpan-4.1</h2>
+
+<h3>CD8 Epitope data set</h3>
+<p>
+<a target=_blank HREF="./suppl/CD8_epitopes/CD8_epitopes.fsa" target=_blank>CD8_epitopes.fsa</a> 
+
+<h3>MS Ligands</h3>
+<p>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:02">HLA-A02:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:05">HLA-A02:05</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:06">HLA-A02:06</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A02:11">HLA-A02:11</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A11:01">HLA-A11:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A23:01">HLA-A23:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A25:01">HLA-A25:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A26:01">HLA-A26:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A30:01">HLA-A30:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A30:02">HLA-A30:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A32:01">HLA-A32:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A33:01">HLA-A33:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A66:01">HLA-A66:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-A68:01">HLA-A68:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B07:02">HLA-B07:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B08:01">HLA-B08:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B14:02">HLA-B14:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:01">HLA-B15:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:02">HLA-B15:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:03">HLA-B15:03</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B15:17">HLA-B15:17</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B18:01">HLA-B18:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B35:03">HLA-B35:03</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B37:01">HLA-B37:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B38:01">HLA-B38:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B40:01">HLA-B40:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B40:02">HLA-B40:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B45:01">HLA-B45:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B46:01">HLA-B46:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B53:01">HLA-B53:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-B58:01">HLA-B58:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C03:03">HLA-C03:03</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C05:01">HLA-C05:01</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C07:02">HLA-C07:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C08:02">HLA-C08:02</a><br>
+<a target=_blank HREF="./suppl/MHCI_ligands/HLA-C12:03">HLA-C12:03</a><br>
+
+
+<h2>NetMHCIIpan-4.0</h2>
+<p>
+<a target=_blank HREF="./suppl/CD4_epitopes/CD4_epitopes.fsa">CD4_epitopes.fsa</a>
+
+<hr>
+
+<h2>References</h2>
+
+<p>
+<b> NetMHCpan-4.1 and NetMHCIIpan-4.0: Improved predictions of MHC antigen presentation by concurrent motif deconvolution and integration of MS MHC eluted ligand data
+</b><br>Submitted 2020.
+ 
+<!-- SLUT INDHOLD -->
+
+<!-- footer -->
+            
