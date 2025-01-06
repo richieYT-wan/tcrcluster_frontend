25bacb3854f27787abe31b6859d73533b5a7086c
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2025-01-06 13:32:34 +0100

    Update 6-Evaluation_data_sets.php

--- 6-Evaluation_data_sets.php
+++ 6-Evaluation_data_sets.php
@@ -13,17 +13,18 @@ Here, you will find the data set used for training and evaluating the TCRcluster
 <h2>TCRcluster-1.0</h2>
 
 <h3>Training data</h3>
-<i>Only partitions 2,3,4 were used for training. Partition 0 was used as the validation and partition 1 was used as the held-out test set.</i>
+<i>Only partitions 2,3,4 were used for training.
+<br>Partition 0 was used as the validation.
+<br>Partition 1 was used as the held-out test set.</i>
 <p>
-<a target=_blank HREF="./supp/TCRcluster_train_data_all_partitions_binders_only.csv">All partitions, binders only</a>
-<a target=_blank HREF="./supp/TCRcluster_train_data_all_partitions_with_swap_negs.csv">All partitions, with swapped negatives</a>
+<a target=_blank HREF="./supp/TCRcluster_train_data_all_partitions_binders_only.csv">All partitions, binders only</a><br>
+<a target=_blank HREF="./supp/TCRcluster_train_data_all_partitions_with_swap_negs.csv">All partitions, with swapped negatives</a><br>
 
 <h3>Evaluation data</h3>
 <i>Only partitions 0 and 1 were used for evaluating.
 <p>
 <a target=_blank HREF="./supp/TCRcluster_17peptides_eval_partitions_binders_only.csv">17 peptides subset, binders only</a><br>
 <a target=_blank HREF="./supp/TCRcluster_17peptides_eval_partitions_with_swap_negs.csv">17 peptides subset, with swapped negatives</a><br>
-<
 
 
 <h2>References</h2>
