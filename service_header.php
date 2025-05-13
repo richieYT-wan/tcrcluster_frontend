6b6ddd3de8f4f084984f216c9d40034dd3f950c0
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2025-05-13 14:02:54 +0200

    Update service_header.php

--- service_header.php
+++ service_header.php
@@ -1,2 +1,3 @@
 <p>TCRcluster is a variational autoencoder and agglomerative clustering pipeline. It clusters TCRs together based on latent cosine distances extracted with a VAE.</p>
 <p>If your file is properly formatted and you encounter an error, it is likely that you have duplicates TCRs in your dataset. TCRCluster-1.0 is not meant to handle exact duplicates.</p>
+<p>TCRCluster: A Novel Approach to T-Cell Receptor latent featurization and Clustering Using Contrastive Learning-Guided Two-Stage Variational Autoencoders, DOI: 10.1093/nargab/lqaf065</p>
